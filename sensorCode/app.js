654a8179c56f4cb2f70d10b015b76ddc21de3f23
Karthikg09 <karthikgacharya@gmail.com>
2023-03-26 22:07:32 +0530

    initial commit

---
+++ sensorCode/app.js
@@ -0,0 +1,84 @@
+setInterval(function() {
+    // Call a function repetatively with 2 Second interval
+    getTemperatureData();
+    getHumidityData();
+    getAirQualityData();
+  }, 2000); 
+  
+  setInterval(function() {
+    // Call a function repetatively with 1 Second interval
+    Time_Date();
+  }, 1000); 
+
+  function getTemperatureData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("TemperatureValue").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "readTemperature", true);
+    xhttp.send();
+  }
+
+  function getHumidityData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("HumidityValue").innerHTML =
+      this.responseText;
+      }
+    };
+    xhttp.open("GET", "readHumidity", true);
+    xhttp.send();
+  }
+
+  function getAirQualityData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("AirQualityValue").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "readGas", true);
+    xhttp.send();
+  }
+
+  function sendData(pos1, pos2) {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
+  }
+};
+xhttp.open("GET", "setPOS?servoPOS1="+pos1+"&servoPOS2="+pos2, true);
+xhttp.send();
+} 
+  var slider1 = document.getElementById("myRange1");
+  var output1 = document.getElementById("demo1");
+  output1.innerHTML = slider1.value;
+  slider1.oninput = function() {
+    output1.innerHTML = this.value;
+    sendData(output1.innerHTML, output2.innerHTML);
+  }  
+  var slider2 = document.getElementById("myRange2");
+  var output2 = document.getElementById("demo2");
+  output2.innerHTML = slider2.value;
+  slider2.oninput = function() {
+    output2.innerHTML = this.value;
+    sendData(output1.innerHTML, output2.innerHTML);
+  }        
+       
+
+
+
+  function Time_Date() {
+    var t = new Date();
+    document.getElementById("time").innerHTML = t.toLocaleTimeString();
+    var d = new Date();
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+    const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    document.getElementById("date").innerHTML = dayNames[d.getDay()] + ", " + d.getDate() + "-" + monthNames[d.getMonth()] + "-" + d.getFullYear();
+  }
